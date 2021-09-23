58b86280e0eae2e78d499a43205ffaec48c632d3
EgorDanilenko <egordanilenkooo@gmail.com>
2021-09-23 12:12:43 +0300

    1.1

--- src/com/company/Footballer.java
+++ src/com/company/Footballer.java
@@ -12,40 +12,40 @@ public class Footballer {
         public final int MAX_YEARSOLD=60;
         public final int MAX_GAMES=1500, MAX_GOALS=2000;
         public void SetYearsold(byte yearsold){
-                byte correctyearsold=yearsold;
-                if(correctyearsold<MIN_YEARSOLD || correctyearsold>MAX_YEARSOLD){
+                byte correctYearsOld=yearsold;
+                if(correctYearsOld<MIN_YEARSOLD || correctYearsOld>MAX_YEARSOLD){
                         do{
                                 System.out.println("Try again. Input yearsold:");
-                        correctyearsold=in.nextByte();
-                        }while(correctyearsold<MIN_YEARSOLD || correctyearsold>MAX_YEARSOLD);
+                        correctYearsOld=in.nextByte();
+                        }while(correctYearsOld<MIN_YEARSOLD || correctYearsOld>MAX_YEARSOLD);
                 }
 
-                this.yearsold = correctyearsold;
+                this.yearsold = correctYearsOld;
         }
         public byte GetYearsold(){
                 return yearsold;
         }
         public void SetNumbergames(short numbergames){
-                short correctnumbergames=numbergames;
-                if(correctnumbergames<0 || correctnumbergames>MAX_GAMES){
+                short correctNumberGames=numbergames;
+                if(correctNumberGames<0 || correctNumberGames>MAX_GAMES){
                         do {System.out.print("Try again. Input numbergames:");
-                        correctnumbergames=in.nextShort();
-                        }while(correctnumbergames<0 || correctnumbergames>MAX_GAMES);
+                        correctNumberGames=in.nextShort();
+                        }while(correctNumberGames<0 || correctNumberGames>MAX_GAMES);
                 }
 
-                this.numbergames=correctnumbergames;
+                this.numbergames=correctNumberGames;
         }
         public short GetNumbergames(){
                 return numbergames;
         }
         public void SetNumbergoals(short numbergoals){
-                short correctnumbergoals=numbergoals;
-                if(correctnumbergoals<0 || correctnumbergoals>MAX_GAMES){
+                short correctNumberGoals=numbergoals;
+                if(correctNumberGoals<0 || correctNumberGoals>MAX_GAMES){
                         System.out.println("Try again. Input numbergoals:");
-                        correctnumbergoals=in.nextShort();
+                        correctNumberGoals=in.nextShort();
                 }
 
-                this.numbergoals=correctnumbergoals;
+                this.numbergoals=correctNumberGoals;
         }
         public short GetNumbergoals(){
                 return numbergoals;
